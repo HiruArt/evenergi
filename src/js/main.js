7c03ef3e60f94630c9c2deff411971b606638e04
HiruArt <girukartem@gmail.com>
2021-06-11 00:13:18 +0300

    night beach

--- src/js/main.js
+++ src/js/main.js
@@ -233,6 +233,56 @@ $(document).ready(function () {
 		$("[data-tab-content="+ dataTabContentName + "]").addClass("active");
 	});
 
+	//script for counter
+	if ($('[data-max-count]').length > 0) {
+
+		function animateValue(id, start, end, duration) {
+			if (start === end) return;
+			let range = end - start;
+			let current = start;
+			let increment = end > start ? 1 : -1;
+			let stepTime = Math.abs(Math.floor(duration / range));
+			let obj = document.getElementById(id);
+			let timer = setInterval(function () {
+				current += increment;
+				obj.innerHTML = current;
+				if (current == end) {
+					clearInterval(timer);
+				}
+			}, stepTime);
+		}
+
+		function showCounterChange() {
+			let counterNum = $('[data-max-count]');
+			counterNum.each(function () {
+				let id = $(this).attr('id');
+				let end = $(this).attr('data-max-count')
+				animateValue(id, 0, end, 2000);
+			});
+		}
+
+		let counterOffsetTop = $('[data-max-count]').offset().top;
+		let screenHeight = $(window).height();
+		let counterFlag = true;
+
+		$(window).scroll(() => {
+			counterOffsetTop = $('[data-max-count]').offset().top - screenHeight;
+
+			if (counterOffsetTop < $(document).scrollTop()
+				&& counterFlag) {
+				showCounterChange();
+				counterFlag = false;
+			}
+		});
+
+
+		if (counterOffsetTop < $(document).scrollTop() + 300
+			&& counterFlag) {
+			showCounterChange();
+			counterFlag = false;
+		}
+	}
+
 
 	// checking browser for WEBP
 	hasWebP().then(function () {
